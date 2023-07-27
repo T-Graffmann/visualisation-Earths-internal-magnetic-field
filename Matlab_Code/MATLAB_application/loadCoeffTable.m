7c62d3d2cffc56046a743de2e14fe6f9286d3f08
T-Graffmann <140721662+T-Graffmann@users.noreply.github.com>
2023-07-27 16:19:25 +0200

    Delete Matlab_Code/MATLAB_application directory

--- Matlab_Code/MATLAB_application/loadCoeffTable.m
+++
@@ -1,8 +0,0 @@
-function Koeff = loadCoeffTable(Tablename)
-
-%loadCoeff loads table of coefficients provided by the IGRF versions. It is
-%given the name provided in the central life script in order to account for
-% different names under which tables are safed. 
-
-Koeff=readtable(Tablename);
-end
